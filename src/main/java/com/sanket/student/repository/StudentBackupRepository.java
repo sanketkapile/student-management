55d6c67b9cfa942b0d13ae078ad49922cabd60f6
SANKET KAPILE <sanketkapile1090@gmail.com>
2024-07-28 02:04:53 +0100

    error: delete fuction added backup repository and class failed

---
+++ src/main/java/com/sanket/student/repository/StudentBackupRepository.java
@@ -0,0 +1,9 @@
+package com.sanket.student.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sanket.student.entity.StudentBackup;
+
+public interface StudentBackupRepository extends JpaRepository<StudentBackup, Long>{
+
+}
--- src/main/java/com/sanket/student/service/impl/StudentServiceImpl.java
+++ src/main/java/com/sanket/student/service/impl/StudentServiceImpl.java
@@ -4,23 +4,34 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sanket.banking.mapper.AccountMapper;
 import com.sanket.student.dto.StudentDto;
 import com.sanket.student.entity.Student;
+import com.sanket.student.entity.StudentBackup;
 import com.sanket.student.mapper.StudentMapper;
+import com.sanket.student.repository.StudentBackupRepository;
 import com.sanket.student.repository.StudentRepository;
 import com.sanket.student.service.StudentService;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 
+	
 	private StudentRepository studentRepository;
+	private StudentBackupRepository studentBackupRepository;
 
+	@Autowired
 	public StudentServiceImpl(StudentRepository studentRepository) {
 		super();
 		this.studentRepository = studentRepository;
+	}
+	@Autowired
+	public StudentServiceImpl(StudentBackupRepository studentBackupRepository) {
+		super();
+		this.studentBackupRepository = studentBackupRepository;
 	}
 
 	@Override
@@ -47,8 +58,13 @@ public class StudentServiceImpl implements StudentService {
 	@Override
 	public void deleteAccount(Long id) {
 		Student student = studentRepository.findById(id).orElseThrow(() -> new RuntimeException("Student Record Not Found"));
-		student.setIsStudent("Inactive");
-		studentRepository.save(student);
+		StudentBackup studentBackup = new StudentBackup();
+		studentBackup.setId(student.getId());
+		studentBackup.setIsStudent(student.getIsStudent());
+		studentBackup.setStudentName(student.getStudentName());
+		studentBackup.setStudyYear(student.getStudyYear());
+		studentRepository.delete(student);
+		studentBackupRepository.save(studentBackup);
 	}
 
 }
